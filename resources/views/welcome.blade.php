ed74dc91957794348f4967e0f27dc6e029887c4f
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-25 15:58:27 +0600

    update homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,22 +22,23 @@
 </head>
 
 
-<body class="bg-gradient-to-r from-slate-50 via-purple-50 to-zinc-50">
+<body class="bg-gradient-to-r from-slate-50 via-purple-50 to-zinc-50 ">
 
 <!-- navbar edits -->
 
 <nav class="bg-white border-gray-200 sticky top-0 left-0 w-full z-50">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-center px-20 mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo"/>
-            <span class="self-center text-2xl font-semibold whitespace-nowrap ">Company</span>
+   
+        <a href="http://marinerx.co.uk/assets/images/footer-logo.png" class="flex items-center">
+            <img src="http://marinerx.co.uk/assets/images/logo.png" class="h-10 mr-3" alt="Flowbite Logo"/>
+           
         </a>
 
     </div>
 </nav>
 
 <!-- rownok edits -->
-<div class="container mx-auto my-10">
+<div class="relative container mx-auto min-h-[400px] mb-10 mt-20 items-center">
     <!-- Radio buttons -->
     <form class="flex flex-col items-center" action="" method="get">
         <p class="text-xl mb-5 font-semibold">Trace Your Order Here</p>
@@ -121,9 +122,9 @@
         @endphp
         <div class="">
             <div class="mx-4 p-4">
-                <div class="flex items-center">
+                <div class="flex flex-col md:flex-row items-center">
                     <div
-                        class="flex items-center {{ $active >= 1 ? 'text-white' : 'text-blue'}} relative">
+                        class="flex flex-col md:flex-row items-center {{ $active >= 1 ? 'text-white' : 'text-blue'}} relative">
                         <div
                             class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 {{ $active >= 1 ? 'bg-blue-600' : ''}} border-blue-600">
                             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
@@ -145,9 +146,9 @@
                     </div>
                     @if (isset($datas->progress1))
                         <div
-                            class="flex-auto border-t-2 transition duration-500 ease-in-out {{ $active >= 2 ? 'border-orange-600' : 'border-gray-300'}}"></div>
+                            class="flex flex-col md:flex-auto border-t-2 transition duration-500 ease-in-out {{ $active >= 2 ? 'border-orange-600' : 'border-gray-300'}}"></div>
                         <div
-                            class="flex items-center {{ $active >= 2 ? 'text-white' : 'text-black'}} relative">
+                            class="flex flex-col md:flex-row items-center {{ $active >= 2 ? 'text-white' : 'text-black'}} relative">
                             <div
                                 class="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 {{ $active >= 2 ? 'bg-orange-600' : ''}} border-orange-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="20" viewBox="0 0 24 24">
@@ -215,9 +216,9 @@
             </div>
 
 
-            <div class="relative overflow-x-auto my-20">
+            <div class="relative overflow-auto md:my-20">
                 <p class="text-xl my-5 font-semibold">Basic Details</p>
-                <table class="w-full text-sm text-left text-gray-500 border rounded-lg ">
+                <table class="w-full text-sm text-left text-gray-500 border rounded-lg overflow-x-auto">
                     <thead class="text-base text-gray-700 capitalize bg-blue-50">
                     <tr>
                         <th scope="col" class="px-6 py-3">
@@ -258,10 +259,10 @@
                     </tbody>
                 </table>
             </div>
-            <p class="text-xl my-5 font-semibold">Route scheduling</p>
-            <div class="grid grid-cols-3 items-start gap-10 mt-10">
+            <p class="text-xl my-5 font-semibold pl-10 md:pl-0">Route scheduling</p>
+            <div class="grid grid-cols-1 md:grid-cols-3 items-center md:items-start gap-10 mt-10 px-10">
 
-                <ol class="relative text-gray-500 border-l border-gray-200 col-span-1">
+                <ol class="relative text-gray-500 border-l border-gray-200 md:col-span-1">
                     @if(isset($datas->status))
                         @foreach ($datas->status->reverse() as $status)
                             <li class="{{ $loop->last ? '' : 'mb-8' }} ml-6">
@@ -276,7 +277,7 @@
                                                                       d="M1 5.917 5.724 10.5 15 1.5"/>
                                                             </svg>
                                                         </span>
-                                <h3 class="font-medium leading-tight font-semibold">
+                                <h3 class="font-medium leading-tight ">
                                     <strong>{{ $status->title }}</strong></h3>
                                 <p class="text">{{ $status->description }}</p>
                                 <p class='text'>{{ \Carbon\Carbon::createFromFormat('Y-m-d', $status->time)->format('jS F, Y') }}</p>
@@ -285,7 +286,7 @@
                         @endforeach
                     @endif
                 </ol>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-5 col-span-2">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-5 md:col-span-2">
                     <div class="bg-teal-100 border border-teal-400 px-4 py-2 flex items-center shadow-md rounded-lg">
                         <p>Vessel Voy No. : {{ $datas->vessel_voy_no }}</p>
                     </div>
@@ -309,7 +310,7 @@
                         <p>Gross Cargo Weight : {{ $datas->gross_cargo_weight }} </p>
                     </div>
                     <div
-                        class="bg-orange-100 border border-orange-400 px-4 py-2 col-span-3  flex justify-centeritems-center shadow-md rounded-lg">
+                        class="bg-orange-100 border border-orange-400 px-4 py-2 md:col-span-3  flex justify-center items-center shadow-md rounded-lg">
                         <p>Service Requirement : {{ $datas->service_requirement }}</p>
                     </div>
                 </div>
@@ -382,103 +383,26 @@
 </div>
 
 <!-- footer area -->
-<div class="inset-x-0 bottom-0 px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-20" style="background-color: #9ca8b3">
-    <div class="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
-        <div class="sm:col-span-2">
-            <a href="/" aria-label="Go home" title="Company" class="inline-flex items-center">
-                <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round"
-                     stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-                    <rect x="3" y="1" width="7" height="12"></rect>
-                    <rect x="3" y="17" width="7" height="6"></rect>
-                    <rect x="14" y="1" width="7" height="6"></rect>
-                    <rect x="14" y="11" width="7" height="12"></rect>
-                </svg>
-                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
-            </a>
-            <div class="mt-6 lg:max-w-sm">
-                <p class="text-sm text-gray-800">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                    totam rem aperiam.
-                </p>
-                <p class="mt-4 text-sm text-gray-800">
-                    Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                </p>
-            </div>
-        </div>
-        <div class="space-y-2 text-sm">
-            <p class="text-base font-bold tracking-wide text-gray-900">Contacts</p>
-            <div class="flex">
-                <p class="mr-1 text-gray-800">Phone:</p>
-                <a href="tel:850-123-5021" aria-label="Our phone" title="Our phone"
-                   class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">850-123-5021</a>
-            </div>
-            <div class="flex">
-                <p class="mr-1 text-gray-800">Email:</p>
-                <a href="mailto:info@lorem.mail" aria-label="Our email" title="Our email"
-                   class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">info@lorem.mail</a>
-            </div>
-            <div class="flex">
-                <p class="mr-1 text-gray-800">Address:</p>
-                <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" aria-label="Our address"
-                   title="Our address"
-                   class="transition-colors duration-300 text-deep-purple-accent-400 hover:text-deep-purple-800">
-                    312 Lovely Street, NY
-                </a>
-            </div>
-        </div>
-        <div>
-            <span class="text-base font-bold tracking-wide text-gray-900">Social</span>
-            <div class="flex items-center mt-1 space-x-3">
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
-                        <path
-                            d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
-                        ></path>
-                    </svg>
-                </a>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
-                        <circle cx="15" cy="15" r="4"></circle>
-                        <path
-                            d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z"
-                        ></path>
-                    </svg>
-                </a>
-                <a href="/" class="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400">
-                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
-                        <path
-                            d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
-                        ></path>
-                    </svg>
-                </a>
-            </div>
-            <p class="mt-4 text-sm text-gray-500">
-                Bacon ipsum dolor amet short ribs pig sausage prosciutto chicken spare ribs salami.
-            </p>
-        </div>
-    </div>
-    <div class="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-        <p class="text-sm text-gray-600">
-            © Copyright 2020 Lorem Inc. All rights reserved.
-        </p>
-        <ul class="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-            <li>
-                <a href="/"
-                   class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">F.A.Q</a>
-            </li>
-            <li>
-                <a href="/"
-                   class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Privacy
-                    Policy</a>
-            </li>
-            <li>
-                <a href="/"
-                   class="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400">Terms
-                    &amp; Conditions</a>
-            </li>
-        </ul>
-    </div>
+<div class="bg-[#002B35] ">
+
+    <div class="flex flex-col justify-center items-center px-10 md:px-20 pt-20 text-white">
+    <img src="http://marinerx.co.uk/assets/images/footer-logo.png" class="w-1/2" alt="logo"/>
+<p class="text-white text-justify">Welcome to MarinerX Logistics, your ultimate partner in seamless cargo shipping and transportation solutions. With a dedication to excellence, we navigate the seas with precision, delivering your goods safely and swiftly across the globe. Trust MarinerX for efficient, reliable, and expert logistics services.At MarinerX, our experienced team ensures efficient customs clearance and advanced tracking, guaranteeing peace of mind throughout the entire shipping process. Experience excellence with MarinerX Logistics today.</p>
+<div class="flex gap-2 items-center mt-5">
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z"/><circle cx="12" cy="10" r="3"/></svg>
+<p>Bangladesh </p>
 </div>
+ <div class="w-full h-[1px] mt-5 bg-gray-100"></div>
+ <div class="flex flex-col justify-between items-center w-full my-4">
+                <p>© 2023 Fastrans- Logistics Services. All rights reserved.
+Made by with  Thrive Digital</p>
+<p>made with Trive Day</p>
+ </div>
+    </div>
+
+
+</div>
+
 </body>
 
 </html>
